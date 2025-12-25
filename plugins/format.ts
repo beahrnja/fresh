f3ba29b14d3fd5da987f547ee81ade34a8b38457
Bea Hrnja <bhrnja.mail@gmail.com>
2025-12-25 18:10:36 -0500

    Removed plugins

--- plugins/format.ts
+++
@@ -1,9 +0,0 @@
-import { fresh } from "fresh";
-
-fresh.commands.add("format_file", async () => {
-  const path = await fresh.activeBuffer.getPath();
-  if (path) {
-    await fresh.terminal.run(`prettier --write "${path}"`);
-    await fresh.activeBuffer.reload();
-  }
-});
